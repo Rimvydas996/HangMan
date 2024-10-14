2944ba04dc80f6e107983f7a60ebbaaa41490a80
rimvydas996 <rimvydasradziukynas@gmail.com>
2024-10-14 17:15:40 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+const wordsToGessArray: object[] = [
+  {
+    word: "hello",
+    letters: ["h", "e", "l", "l", "o"],
+  },
+  {
+    word: "world",
+    letters: ["w", "o", "r", "l", "d"],
+  },
+];
+interface IWordToGuess {
+  word: string;
+  letters: string[];
+}
